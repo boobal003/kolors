4a29472542d486f3d015eff9e0ab452d7ecd3ea9
Boobalan <141622627+boobal003@users.noreply.github.com>
2024-08-31 15:59:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,265 @@
-import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
-
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+import os
+import cv2
+import spaces
+from PIL import Image
+import gradio as gr
+import numpy as np
+import random
+import base64
+import requests
+import json
+import time
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+def tryon(person_img, garment_img, seed, randomize_seed):
+    post_start_time = time.time()
+    if person_img is None or garment_img is None:
+        return None, None, "Empty image"
+    if randomize_seed:
+        seed = random.randint(0, MAX_SEED)
+    encoded_person_img = cv2.imencode('.jpg', cv2.cvtColor(person_img, cv2.COLOR_RGB2BGR))[1].tobytes()
+    encoded_person_img = base64.b64encode(encoded_person_img).decode('utf-8')
+    encoded_garment_img = cv2.imencode('.jpg', cv2.cvtColor(garment_img, cv2.COLOR_RGB2BGR))[1].tobytes()
+    encoded_garment_img = base64.b64encode(encoded_garment_img).decode('utf-8')
 
-st.header(f'GDP in {to_year}', divider='gray')
+    url = "http://" + os.environ['tryon_url'] + "Submit"
+    token = os.environ['token']
+    cookie = os.environ['Cookie']
+    referer = os.environ['referer']
+    headers = {'Content-Type': 'application/json', 'token': token, 'Cookie': cookie, 'referer': referer}
+    data = {
+        "clothImage": encoded_garment_img,
+        "humanImage": encoded_person_img,
+        "seed": seed
+    }
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data), timeout=50)
+        print("post response code", response.status_code)
+        if response.status_code == 200:
+            result = response.json()['result']
+            status = result['status']
+            if status == "success":
+                uuid = result['result']
+                print(uuid)
+    except Exception as err:
+        print(f"Error: {err}")
+        raise gr.Error("Too many users, please try again later")
+    post_end_time = time.time()
+    print(f"post time used: {post_end_time-post_start_time}")
 
-''
+    get_start_time =time.time()
+    time.sleep(9)
+    Max_Retry = 10
+    result_img = None
+    for i in range(Max_Retry):
+        try:
+            url = "http://" + os.environ['tryon_url'] + "Query?taskId=" + uuid
+            response = requests.get(url, headers=headers, timeout=15)
+            print("get response code", response.status_code)
+            if response.status_code == 200:
+                result = response.json()['result']
+                status = result['status']
+                if status == "success":
+                    result = base64.b64decode(result['result'])
+                    result_np = np.frombuffer(result, np.uint8)
+                    result_img = cv2.imdecode(result_np, cv2.IMREAD_UNCHANGED)
+                    result_img = cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR)
+                    info = "Success"
+                    break
+                elif status == "error":
+                    raise gr.Error("Too many users, please try again later")
+            else:
+                print(response.text)
+                info = "URL error, pleace contact the admin"
+        except requests.exceptions.ReadTimeout:
+            print("timeout")
+            info = "Too many users, please try again later"
+        except Exception as err:
+            print(f"Error: {err}")
+        time.sleep(1)
+    get_end_time = time.time()
+    print(f"get time used: {get_end_time-get_start_time}")
 
-cols = st.columns(4)
+    return result_img, seed, info
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+def start_tryon(person_img, garment_img, seed, randomize_seed):
+    start_time = time.time()
+    if person_img is None or garment_img is None:
+        return None, None, "Empty image"
+    if randomize_seed:
+        seed = random.randint(0, MAX_SEED)
+    encoded_person_img = cv2.imencode('.jpg', cv2.cvtColor(person_img, cv2.COLOR_RGB2BGR))[1].tobytes()
+    encoded_person_img = base64.b64encode(encoded_person_img).decode('utf-8')
+    encoded_garment_img = cv2.imencode('.jpg', cv2.cvtColor(garment_img, cv2.COLOR_RGB2BGR))[1].tobytes()
+    encoded_garment_img = base64.b64encode(encoded_garment_img).decode('utf-8')
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+    url = "http://" + os.environ['tryon_url']
+    token = os.environ['token']
+    cookie = os.environ['Cookie']
+    referer = os.environ['referer']
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
+    headers = {'Content-Type': 'application/json', 'token': token, 'Cookie': cookie, 'referer': referer}
+    data = {
+        "clothImage": encoded_garment_img,
+        "humanImage": encoded_person_img,
+        "seed": seed
+    }
+
+    result_img = None
+    try:
+        session = requests.Session()
+        response = session.post(url, headers=headers, data=json.dumps(data), timeout=60)
+        print("response code", response.status_code)
+        if response.status_code == 200:
+            result = response.json()['result']
+            status = result['status']
+            if status == "success":
+                result = base64.b64decode(result['result'])
+                result_np = np.frombuffer(result, np.uint8)
+                result_img = cv2.imdecode(result_np, cv2.IMREAD_UNCHANGED)
+                result_img = cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR)
+                info = "Success"
+            else:
+                info = "Try again latter"
         else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+            print(response.text)
+            info = "URL error, pleace contact the admin"
+    except requests.exceptions.ReadTimeout:
+        print("timeout")
+        info = "Too many users, please try again later"
+        raise gr.Error("Too many users, please try again later")
+    except Exception as err:
+        print(f"其他错误: {err}")
+        info = "Error, pleace contact the admin"
+    end_time = time.time()
+    print(f"time used: {end_time-start_time}")
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
+    return result_img, seed, info
+
+MAX_SEED = 999999
+
+example_path = os.path.join(os.path.dirname(__file__), 'assets')
+
+garm_list = os.listdir(os.path.join(example_path,"cloth"))
+garm_list_path = [os.path.join(example_path,"cloth",garm) for garm in garm_list]
+
+human_list = os.listdir(os.path.join(example_path,"human"))
+human_list_path = [os.path.join(example_path,"human",human) for human in human_list]
+
+css="""
+#col-left {
+    margin: 0 auto;
+    max-width: 430px;
+}
+#col-mid {
+    margin: 0 auto;
+    max-width: 430px;
+}
+#col-right {
+    margin: 0 auto;
+    max-width: 430px;
+}
+#col-showcase {
+    margin: 0 auto;
+    max-width: 1100px;
+}
+#button {
+    color: blue;
+}
+"""
+
+def load_description(fp):
+    with open(fp, 'r', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+def change_imgs(image1, image2):
+    return image1, image2
+
+with gr.Blocks(css=css) as Tryon:
+    gr.HTML(load_description("assets/title.md"))
+    with gr.Row():
+        with gr.Column(elem_id = "col-left"):
+            gr.HTML("""
+            <div style="display: flex; justify-content: center; align-items: center; text-align: center; font-size: 20px;">
+                <div>
+                Step 1.  Upload a person image ⬇️
+                </div>
+            </div>
+            """)
+        with gr.Column(elem_id = "col-mid"):
+            gr.HTML("""
+            <div style="display: flex; justify-content: center; align-items: center; text-align: center; font-size: 20px;">
+                <div>
+                Step 2. Upload a garment image ⬇️
+                </div>
+            </div>
+            """)
+        with gr.Column(elem_id = "col-right"):
+            gr.HTML("""
+            <div style="display: flex; justify-content: center; align-items: center; text-align: center; font-size: 20px;">
+                <div>
+                Step 3. Press “Run” to get try-on results
+                </div>
+            </div>
+            """)
+    with gr.Row():
+        with gr.Column(elem_id = "col-left"):
+            imgs = gr.Image(label="Person image", sources='upload', type="numpy")
+            # category = gr.Dropdown(label="Garment category", choices=['upper_body', 'lower_body', 'dresses'],  value="upper_body")
+            example = gr.Examples(
+                inputs=imgs,
+                examples_per_page=12,
+                examples=human_list_path
+            )
+        with gr.Column(elem_id = "col-mid"):
+            garm_img = gr.Image(label="Garment image", sources='upload', type="numpy")
+            example = gr.Examples(
+                inputs=garm_img,
+                examples_per_page=12,
+                examples=garm_list_path
+            )
+        with gr.Column(elem_id = "col-right"):
+            image_out = gr.Image(label="Result", show_share_button=False)
+            with gr.Row():
+                seed = gr.Slider(
+                    label="Seed",
+                    minimum=0,
+                    maximum=MAX_SEED,
+                    step=1,
+                    value=0,
+                )
+                randomize_seed = gr.Checkbox(label="Random seed", value=True)
+            with gr.Row():
+                seed_used = gr.Number(label="Seed used")
+                result_info = gr.Text(label="Response")
+            # try_button = gr.Button(value="Run", elem_id="button")
+            test_button = gr.Button(value="Run", elem_id="button")
+
+
+    # try_button.click(fn=start_tryon, inputs=[imgs, garm_img, seed, randomize_seed], outputs=[image_out, seed_used, result_info], api_name='tryon',concurrency_limit=10)
+    test_button.click(fn=tryon, inputs=[imgs, garm_img, seed, randomize_seed], outputs=[image_out, seed_used, result_info], api_name='tryon', concurrency_limit=40)
+
+    with gr.Column(elem_id = "col-showcase"):
+        gr.HTML("""
+        <div style="display: flex; justify-content: center; align-items: center; text-align: center; font-size: 20px;">
+            <div> </div>
+            <br>
+            <div>
+            Virtual try-on examples in pairs of person and garment images
+            </div>
+        </div>
+        """)
+        show_case = gr.Examples(
+            examples=[
+                ["assets/examples/model2.png", "assets/examples/garment2.png", "assets/examples/result2.png"],
+                ["assets/examples/model3.png", "assets/examples/garment3.png", "assets/examples/result3.png"],
+                ["assets/examples/model1.png", "assets/examples/garment1.png", "assets/examples/result1.png"],
+            ],
+            inputs=[imgs, garm_img, image_out],
+            label=None
         )
+
+# ip = requests.get('http://ifconfig.me/ip', timeout=1).text.strip()
+# print("ip address", ip)
+Tryon.launch()
